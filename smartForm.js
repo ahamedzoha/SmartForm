7a043b318e33f5cd825f1dfd66d83ec5fb01a6e6
abyaz878 <1410497@iub.edu.bd>
2018-02-12 09:24:16 +0600

    End class commit

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,26 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+  name:'',
+  email:'',
+};
+$("#start").click(function(){
+  $("#welcome").hide();
+  $("#q1").show();
+});
+
+$("#name").change(function(event){
+  //console.log(event);
+  console.log($("#name").val());
+});
+$("#q1next").click(function(){
+  if($('#name').val() && $('#email').val()){
+    userData.name = $('#name').val();
+    userData.email = $('#email').val();
+    console.log(userData);
+    $("#q2").show();
+    $("#q1").hide();
+  }else {
+    alert("Please enter name and email");
+  }
+});
